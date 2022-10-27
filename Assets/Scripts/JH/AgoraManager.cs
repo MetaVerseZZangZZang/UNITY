a4c6a9e2fc0296f362a1f5174c61032aefd97642
juliajh <juliajuhee222@gmail.com>
2022-10-27 19:36:12 +0900

    char panel 캠 완료

--- Assets/Scripts/JH/AgoraManager.cs
+++ Assets/Scripts/JH/AgoraManager.cs
@@ -43,12 +43,12 @@ public class AgoraManager : MonoBehaviour
         Instance = this;
     }
     // Start is called before the first frame update
-    public void AgoraStart()
+
+    void Start()
     {
         SetupVideoSDKEngine();
         InitEventHandler();
         SetupUI();
-        Join();
     }
 
     // Update is called once per frame
@@ -82,22 +82,7 @@ public class AgoraManager : MonoBehaviour
             FriendCams.transform.GetChild(i).GetComponent<VideoSurface>().transform.Rotate(0.0f, 0.0f, 0f);
             FriendCamList.Add(FriendCams.transform.GetChild(i).GetComponent<VideoSurface>());
         }
-        
-        
-        /*
-        foreach (GameObject cam in friendCamPosList)
-        {
-            RemoteView = cam.AddComponent<VideoSurface>();
-            RemoteView.transform.Rotate(0.0f, 0.0f, 180.0f);
-        }*/
-        
-        //go = GameObject.Find("Leave");
-        //go.GetComponent<Button>().onClick.AddListener(Leave);
-        
-        //go = GameObject.Find("Join");
-        //go.GetComponent<Button>().onClick.AddListener(Join);
 
-        
     }
 
 
@@ -148,8 +133,14 @@ public class AgoraManager : MonoBehaviour
             RemoteView.SetEnable(false);
         // Stops rendering the local video.
         LocalView.SetEnable(false);
-        UI_MainPanel.Instance.Hide();
-        UI_CharPanel.Instance.Show();
+        
+        for (int i = 0; i < FriendCams.transform.childCount; i++)
+        {
+            Destroy(FriendCams.transform.GetChild(i));
+        }
+        
+        FriendCamList.Clear();
+        
     }
 
 
--- Assets/Scripts/JH/UI_MainPanel.cs
+++ Assets/Scripts/JH/UI_MainPanel.cs
@@ -21,6 +21,7 @@ public class UI_MainPanel : MonoBehaviour
         AgoraManager.Instance.Leave();
         PhotonNetwork.LeaveRoom();
         Hide();
+        UI_CharPanel.Instance.Show();
     }
     
     public void Hide()
@@ -30,8 +31,8 @@ public class UI_MainPanel : MonoBehaviour
 
     public void Show()
     {
-        UI_CharPanel.Instance.StopCam();
         this.gameObject.SetActive(true);
-        
+        UI_CharPanel.Instance.StopCam();
+        AgoraManager.Instance.Join();
     }
 }
