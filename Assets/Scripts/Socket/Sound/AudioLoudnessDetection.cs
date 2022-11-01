e5ad49d4998ae5e2147bbbfe55df43cdf13bf62d
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-11-01 16:53:06 +0900

    스크립트 정리

--- Assets/Scripts/Socket/Sound/AudioLoudnessDetection.cs
+++ Assets/Scripts/Socket/Sound/AudioLoudnessDetection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class AudioLoudnessDetection : MonoBehaviour
     public AudioClip microphoneClip;
 
 
-    //마이크 옵션값
+    //마이크 옵션
     public bool recording = false;
     public int inputSoundSensibility = 20;
     public int stopSoundSensibility = 100;
@@ -74,19 +75,53 @@ public class AudioLoudnessDetection : MonoBehaviour
         {
             recording = true;
             Debug.Log("음성 녹음 해야대");
-
-            
         }
 
         if (loudness * stopSoundSensibility <= 0.03f && recording == true)
         {
-            //StartCoroutine(Recording());
             recording = false;
             Debug.Log("녹음 중지");
         }
 
-        return totalLoudness / sampleWindow;
+        return ;
 
+    }
+
+
+    // audioclip to byte
+    public static byte[] GetClipData(AudioClip _clip)
+    {
+        //Get data
+        float[] floatData = new float[_clip.samples * _clip.channels];
+        _clip.GetData(floatData, 0);
+
+        //convert to byte array
+        byte[] byteData = new byte[floatData.Length * 4];
+        Buffer.BlockCopy(floatData, 0, byteData, 0, byteData.Length);
+
+        return (byteData);
+    }
+
+
+
+
+    //Byte to audioclip (바이트가 변환이 이상하다 싶을때 쓰는 확인 코)
+    public static AudioClip ByteToAudio(byte[] vs)
+    {
+        float[] samples = new float[vs.Length / 4]; //size of a float is 4 bytes
+
+        Buffer.BlockCopy(vs, 0, samples, 0, vs.Length);
+
+        int channels = 1; //Assuming audio is mono because microphone input usually is
+        int sampleRate = 44100; //Assuming your samplerate is 44100 or change to 48000 or whatever is appropriate
+
+
+        print(samples.Length);
+        AudioClip clip = AudioClip.Create("ClipName", samples.Length, channels, sampleRate, false);
+        clip.SetData(samples, 0);
+
+
+        return clip;
     }
 
 }
