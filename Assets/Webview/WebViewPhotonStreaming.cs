0d25017513c816a539c0f54dcfa30da73bbbb79c
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-11-04 17:57:51 +0900

    webview photon 수정중

---
+++ Assets/Webview/WebViewPhotonStreaming.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class WebViewPhotonStreaming : MonoBehaviour
+{
+    public PhotonView pv;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+    void DirRPC()
+    {
+
+    }
+}
