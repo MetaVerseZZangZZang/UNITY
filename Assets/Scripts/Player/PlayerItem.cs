dac3efa457dd211fc15ed9cadc00dea7f43ba032
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-11-21 18:10:08 +0900

    asd

--- Assets/Scripts/Player/PlayerItem.cs
+++ Assets/Scripts/Player/PlayerItem.cs
@@ -88,21 +88,21 @@ public class PlayerItem : MonoBehaviour, IPunObservable
                 transform.rotation=Quaternion.Euler(0,90,0);
                 transform.Translate(0, 0, 2f * Time.deltaTime);
 
-                playerAnim.SetTrigger("walkTrigger");
+                //playerAnim.SetBool("walkTrigger",true);
             }
 
             if (axis_X == -1)   //왼
             {
                 transform.rotation=Quaternion.Euler(0,-90,0);
                 transform.Translate(0, 0, 2f * Time.deltaTime);
-                playerAnim.SetTrigger("walkTrigger");
+                //playerAnim.SetBool("walkTrigger", true);
             }
 
             if (axis_Y == 1)   //위
             {
                 transform.rotation=Quaternion.Euler(0,0,0);
                 transform.Translate(0, 0, 2f * Time.deltaTime);
-                playerAnim.SetTrigger("walkTrigger");
+                //playerAnim.SetTrigger("walkTrigger");
 
             }
 
@@ -110,13 +110,18 @@ public class PlayerItem : MonoBehaviour, IPunObservable
             {
                 transform.rotation=Quaternion.Euler(0,180,0);
                 transform.Translate(0, 0, 2f * Time.deltaTime);
-                playerAnim.SetTrigger("walkTrigger");
+                //playerAnim.SetTrigger("walkTrigger");
             }
 
 
             if (axis_X == 0 && axis_Y == 0 ) 
             {
-                playerAnim.ResetTrigger("walkTrigger");
+                playerAnim.SetBool("walk",true);
+            }
+
+            else
+            {
+                playerAnim.SetBool("walk", true);
             }
 
         }
